3041ecf834d21eae6e50388a9f7b27f51555b205
erindrummond <e.b.drummond@gmail.com>
2013-06-08 22:02:16 -0600

    precompiled assets

--- app/controllers/extras_controller.rb
+++ app/controllers/extras_controller.rb
@@ -1,11 +1,12 @@
 class ExtrasController < ApplicationController
+  respond_to :html, :pdf
 
   def about_me
 
   end
 
   def resume
-
+    send_file("public/Erin_Drummond_resume.pdf", :type => 'application/pdf', :disposition => 'inline')
   end
 
   def portfolio
